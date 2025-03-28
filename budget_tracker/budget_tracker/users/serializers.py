77bcee9b48fbd251d253aca5b90d26f20dfe0783
Eyuel763 <eyuelg1997@gmail.com>
2025-03-28 23:36:39 +0400

    restarting the whole project

--- budget_tracker/budget_tracker/users/serializers.py
+++ budget_tracker/budget_tracker/users/serializers.py
@@ -1,16 +1,35 @@
 from rest_framework import serializers
-from .models import CustomUser
+from django.contrib.auth import get_user_model, authenticate
+
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
-        fields =('id', 'email', 'first_name', 'last_name','password', 'is_staff', 'is_active', 'date_joined')
+        model = User
+        fields = ('id', 'username', 'password', 'email')
         extra_kwargs = {'password': {'write_only': True}}
 
-        def create(self, validated_data):
-            print(f"Password before hashing: {validated_data['password']}")
-            user = CustomUser.objects.create_user(**validated_data)
-            user.set_password(validated_data['password'])
-            print(f"Password after hashing: {user.password}")
-            user.save()
-            return user
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        user = User.objects.create_user(**validated_data)
+        if password is not None:
+            user.set_password(password)
+        user.save()
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        username = data.get('username', '')
+        password = data.get('password', '')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user:
+                return data
+            else:
+                raise serializers.ValidationError('Unable to login with provided credentials. ')
+        else:
+            raise serializers.ValidationError('Must include "username" and "password".')
--- budget_tracker/budget_tracker/users/views.py
+++ budget_tracker/budget_tracker/users/views.py
@@ -1,46 +1,27 @@
 from django.shortcuts import render
-from rest_framework import generics
-from .models import CustomUser
-from .serializers import UserSerializer
-from rest_framework.authtoken.models import Token
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserSerializer, LoginSerializer
 from django.contrib.auth import authenticate
-from django.contrib.auth.decorators import login_required
 
-
-
-
-class UserCreateView(generics.CreateAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = UserSerializer
-
-###To registrer user using APIView(POST method)
-#class RegisterView(APIView):
-#    def post(self, request):
-#        serializer = UserSerializer(data=request.data)
-#        if serializer.is_valid():
-#            serializer.save()
-#            return Response(serializer.data, status=status.HTTP_201_CREATED)
-#        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class LoginView(APIView):
-    serializer_class = UserSerializer
+class UserRegistrationView(APIView):
     def post(self, request):
-        email = request.data.get('email')
-        password = request.data.get('password')
-        user = authenticate(email=email, password=password)
-
-        if user:
-            token, created = Token.objects.get_or_create(user=user)
-            return Response({"token": token.key}, status=status.HTTP_200_OK)
-        return Response({"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status= status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-@login_required
-def home(request):
-    return render(request, 'users/home.html', {"user": request.user})
-    
-
+class UserLoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user = authenticate(request, username=serializer.validated_data['username'], password=serializer.validated_data['password'])
+            if user is not None:
+                return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
